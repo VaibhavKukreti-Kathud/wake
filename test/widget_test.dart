d6d8f7005ec4a700503d62a8aa0d31d36a1722e6
Jheel Johari <119886129+jheel23@users.noreply.github.com>
2025-07-29 21:19:33 +0530

    Migration v1 (#1)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,24 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wake/main.dart';
 
 void main() {
+  print('\x1B[32m🧪 Testing Started for Jheel\x1B[0m');
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // // Verify that our counter starts at 0.
+    // expect(find.text('0'), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+  tearDownAll(() {
+    print('\x1B[34m---✅ Test File Completed ✅---\x1B[0m');
   });
 }
